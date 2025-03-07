8c0c5b63f372c7da1678855bc527ee5189eeaf10
John Cosio <mail@jcos.io>
2025-03-07 22:47:59 +0800

    Added kofi form

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -3,7 +3,7 @@
     <div id="mc_embed_signup">
         <form action="https://your-mail-chimp-list-manage-url" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-                <h2>Sign up for our newsletter</h2>
+                <h2>Sign up for newsletter</h2>
                 <div class="mc-field-group">
                     <label for="mce-EMAIL">Email Address </label>
                     <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email address">
@@ -21,9 +21,53 @@
     <!--End Mailchimp Signup Form -->
 </div>
 
+<div class="flex justify-center lg:-mx-12 my-12 p-6 md:px-12 border border-gray-400 text-sm md:rounded shadow">
+    <!-- Begin Support section -->
+    <div id="mc_embed_signup_scroll">
+        <h2>💸 Want to support me?</h2>
+        <p>
+            I offer some of the highest-quality Buyer’s Remorse.
+        </p>
+
+        <div class="text-center cta">
+
+            <a title="{{ $page->siteName }} Support" href="/support"
+               class="text-xl text-[#a9a9b3]">
+                👉 Get Buyer’s Remorse
+            </a>
+
+        </div>
+    </div>
+    <!-- End Support section -->
+</div>
+
 @push('scripts')
     <script src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
     <script>(function($) {
         window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
     </script>
 @endpush
+
+<style>
+    .cta {
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        --tw-text-opacity: 1;
+        color: rgb(169 169 179 / var(--tw-text-opacity, 1));
+        --tw-bg-opacity: 1;
+        background-color: rgb(59 61 66 / var(--tw-bg-opacity, 1));
+        line-height: 1.6;
+        border-radius: 0.25rem;
+        cursor: pointer;
+    }
+
+    .cta:hover a {
+        --tw-text-opacity: 1;
+        color: rgb(185 145 40 / var(--tw-text-opacity, 1));
+        color: #b99128;
+        text-decoration: underline !important;
+        transition: all 0.5s ease 0s;
+    }
+</style>
---
+++ source/support.blade.php
@@ -0,0 +1,20 @@
+---
+title: Support
+description: Support my site
+---
+@extends('_layouts.main')
+
+@section('body')
+    <h1>Support</h1>
+
+    <hr class="border-b my-6">
+
+    <div class="text-left">
+        <h4> Help support me by leaving a tip below.</h4>
+        <h4>Your contribution goes toward server costs, website services, and, most importantly, kilos of rice.</h4>
+        <h4>I strive to provide the best experience of questioning your purchase. 💸💸💸</h4>
+        <h4> Thank you very much! </h4>
+    </div>
+
+    <iframe id='kofiframe' src='https://ko-fi.com/sudoist/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;padding: 2em' height='712' title='sudoist'></iframe>
+@endsection
